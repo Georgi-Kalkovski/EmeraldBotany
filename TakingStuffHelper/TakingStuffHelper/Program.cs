9f4caa2af22929cb8ffed663a8e56f3cd62d5e75
Georgi Kalkovski <Georgi-Kalkovski@users.noreply.github.com>
2020-11-19 20:50:16 +0200

    Adding little prototype console app that can read the species.csv file, which data dump is written on a single column on excel table. In the future the tool will be able to convert the .csv file into the DbModels

---
+++ TakingStuffHelper/TakingStuffHelper/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakingStuffHelper
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Smiley\Desktop\species.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                var list = new List<string[]>();
+
+                while (!parser.EndOfData)
+                {
+                    var sb = new StringBuilder();
+
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    for (int row= 0 ; row < fields.Length-1;)
+                    {
+                        var col = fields[row].Split("\t").ToArray();
+                        foreach (var id in col)
+                        {
+                            sb.Append($"{id} ");
+                        }
+                        list.Add(col);
+                        sb.AppendLine();
+
+                        Console.WriteLine(sb.ToString());
+                        row++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
